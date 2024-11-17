2c15c64c224ddf706280763fb9dde0aa27f7b080
김호연 <yun1094@naver.com>
2024-11-17 11:27:12 +0900

    네비게이션바 구현

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,38 +1,154 @@
 import React, { useState } from 'react';
-import '../App.css'; // CSS 파일 불러오기
+import '../App.css';
 
 const NavigationBar = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  // 네비게이션 전체 열림/닫힘 상태
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsDropdownOpen(true);
+  // 드롭다운 상태를 각각 관리 (공백 및 특수문자 포함)
+  const [dropdownState, setDropdownState] = useState({
+    "교육목표": false,
+    "교과안내": false,
+    "졸업후 진로": false,
+    "학사 일정": false, // 공백 포함
+    "E-행정실": false, // 하이픈 포함
+  });
+
+  // 특정 드롭다운을 열거나 닫는 함수
+  const toggleDropdown = (menu, isOpen) => {
+    setDropdownState((prevState) => ({
+      ...prevState,
+      [menu]: isOpen, // 문자열로 키 접근
+    }));
   };
 
-  const handleMouseLeave = () => {
-    setIsDropdownOpen(false);
+  // 네비게이션 전체 토글 함수
+  const toggleNav = () => {
+    setIsNavOpen((prev) => !prev);
   };
 
   return (
     <nav className="navbar">
+      {/* 네비게이션 바 상단 */}
       <ul className="nav-list">
         <li
           className="nav-item"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
+          onMouseEnter={() => toggleDropdown("교육목표", true)}
+          onMouseLeave={() => toggleDropdown("교육목표", false)}
         >
-          학과소개
-          {isDropdownOpen && (
+          교육목표
+          {dropdownState["교육목표"] && (
             <ul className="dropdown">
-              <li className="dropdown-item">학과장 인사말</li>
-              <li className="dropdown-item">연혁</li>
-              <li className="dropdown-item">교수진 소개</li>
-              <li className="dropdown-item">캠퍼스 소개</li>
+              <li className="dropdown-item">목표 1</li>
+              <li className="dropdown-item">목표 2</li>
+              <li className="dropdown-item">목표 3</li>
             </ul>
           )}
         </li>
-        <li className="nav-item">입학안내</li>
-        <li className="nav-item">커뮤니티</li>
+        <li
+          className="nav-item"
+          onMouseEnter={() => toggleDropdown("교과안내", true)}
+          onMouseLeave={() => toggleDropdown("교과안내", false)}
+        >
+          교과안내
+          {dropdownState["교과안내"] && (
+            <ul className="dropdown">
+              <li className="dropdown-item">과목 1</li>
+              <li className="dropdown-item">과목 2</li>
+              <li className="dropdown-item">과목 3</li>
+            </ul>
+          )}
+        </li>
+        <li
+          className="nav-item"
+          onMouseEnter={() => toggleDropdown("졸업후 진로", true)}
+          onMouseLeave={() => toggleDropdown("졸업후 진로", false)}
+        >
+          졸업후 진로
+          {dropdownState["졸업후 진로"] && (
+            <ul className="dropdown">
+              <li className="dropdown-item">진로 1</li>
+              <li className="dropdown-item">진로 2</li>
+            </ul>
+          )}
+        </li>
+        <li
+          className="nav-item"
+          onMouseEnter={() => toggleDropdown("학사 일정", true)}
+          onMouseLeave={() => toggleDropdown("학사 일정", false)}
+        >
+          학사 일정
+          {dropdownState["학사 일정"] && (
+            <ul className="dropdown">
+              <li className="dropdown-item">일정 1</li>
+              <li className="dropdown-item">일정 2</li>
+            </ul>
+          )}
+        </li>
+        <li
+          className="nav-item"
+          onMouseEnter={() => toggleDropdown("E-행정실", true)}
+          onMouseLeave={() => toggleDropdown("E-행정실", false)}
+        >
+          E-행정실
+          {dropdownState["E-행정실"] && (
+            <ul className="dropdown">
+              <li className="dropdown-item">문서 1</li>
+              <li className="dropdown-item">문서 2</li>
+            </ul>
+          )}
+        </li>
+        {/*햄버거 버튼*/}
+        <div className="hamburger-button" onClick={toggleNav}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
       </ul>
+
+      {/* 햄버거 버튼으로 열리는 전체 드롭다운 */}
+      {isNavOpen && (
+        <div className="expanded-menu">
+          <div className="menu-column">
+            <h3>교육목표</h3>
+            <ul>
+              <li className="dropdown-item">목표 1</li>
+              <li className="dropdown-item">목표 2</li>
+              <li className="dropdown-item">목표 3</li>
+            </ul>
+          </div>
+          <div className="menu-column">
+            <h3>교과안내</h3>
+            <ul>
+              <li className="dropdown-item">과목 1</li>
+              <li className="dropdown-item">과목 2</li>
+              <li className="dropdown-item">과목 3</li>
+            </ul>
+          </div>
+          <div className="menu-column">
+            <h3>졸업후 진로</h3>
+            <ul>
+              <li className="dropdown-item">진로 1</li>
+              <li className="dropdown-item">진로 2</li>
+            </ul>
+          </div>
+          <div className="menu-column">
+            <h3>학사 일정</h3>
+            <ul>
+              <li className="dropdown-item">일정 1</li>
+              <li className="dropdown-item">일정 2</li>
+            </ul>
+          </div>
+          <div className="menu-column">
+            <h3>E-행정실</h3>
+            <ul>
+              <li className="dropdown-item">문서 1</li>
+              <li className="dropdown-item">문서 2</li>
+            </ul>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
